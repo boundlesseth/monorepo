ecaecd3d3f3acfaab80958e0e2d8ddb9a2366b27
proofoftom <tommy.cox@protonmail.com>
2021-03-19 10:49:03 -0600

    Add and configure dotenv

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,8 @@
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
