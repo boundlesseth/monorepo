c3b6dc840fef4964871f37079540a1e3991573fd
Auryn Macmillan <aurynmacmillan@gmail.com>
2021-03-20 20:18:27 -0400

    Started adding superfluid

--- contracts/contracts/Boundless.sol
+++ contracts/contracts/Boundless.sol
@@ -13,16 +13,18 @@ contract Boundless is Ownable, IERC1155, ERC1155Burnable {
     event NewArtistRegistry(address artistRegistry);
     event NewBlockRegistry(address blockRegistry);
     event NewSeller(address seller);
+    event NewReceiver(address receiver);
     event TokenBurned(address account, uint256 id);
     event TokensBurned(address account, uint256[] ids);
     event TokenMinted(bytes32 _blockhash, uint256 id);
 
-    mapping (uint256 => uint) public rates;
+    mapping (uint256 => int96) public rates;
     mapping (uint256 => bool) public minted;
 
     IRegistry public artistRegistry;
     IRegistry public blockRegistry;
     Seller public seller;
+    address public receiver;
 
     // TODO
     constructor(address _artistRegistry, address _blockRegistry, address _seller)
@@ -47,17 +49,6 @@ contract Boundless is Ownable, IERC1155, ERC1155Burnable {
         minted[id] = true;
         seller.sellToken(address(this), id);
         emit TokenMinted(_blockhash, id);
-
-    }
-
-    // get a token id from a block hash and artist ID
-    function getId(bytes32 _blockhash, bytes32 _artist)
-        public
-        pure
-    returns(uint256 id)
-    {
-        id = uint256(keccak256(abi.encode(_blockhash, _artist)));
-        return (id);
     }
 
     // Burn a token
@@ -66,16 +57,39 @@ contract Boundless is Ownable, IERC1155, ERC1155Burnable {
         override
     {
         ERC1155Burnable.burn(account, id, value);
+        minted[id] = false;
         emit TokenBurned(account, id);
     }
 
+    // TODO
     // Burn a batch of tokens
     function burnBatch(address account, uint256[] memory ids, uint256[] memory values)
         public
         override
     {
         ERC1155Burnable.burnBatch(account, ids, values);
+        for (i = 0; ; increase the value of counter;) {
+            Execute multiple instructions here
+        }
         emit TokensBurned(account, ids);
+    }
+
+    // TODO
+    // buy a token from the current owner for the current price
+    function buyToken(address buyer, uint256 id)
+        public
+    {
+        if(balanceOf(address(seller), id) > 1)
+    }
+
+    // get a token id from a block hash and artist ID
+    function getId(bytes32 _blockhash, bytes32 _artist)
+    public
+    pure
+    returns(uint256 id)
+    {
+        id = uint256(keccak256(abi.encode(_blockhash, _artist)));
+        return (id);
     }
 
     // set the artist registry.
@@ -94,6 +108,15 @@ contract Boundless is Ownable, IERC1155, ERC1155Burnable {
     {
         blockRegistry = IRegistry(_blockRegistry);
         emit NewBlockRegistry(address(blockRegistry));
+    }
+
+    // set the receiver.
+    function setReceiver(address _receiver)
+        public
+        onlyOwner
+    {
+        receiver = receiver;
+        emit NewReceiver(address(receiver));
     }
 
     // set the seller.
